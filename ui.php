6166b673ee49b532e34428ccfa0bdb5eca2ca6aa
Bismay <bismay4u@gmail.com>
2015-10-17 10:44:28 +0530

    Added a favicon.png

--- ui.php
+++ ui.php
@@ -7,16 +7,21 @@ $testHead="Logiks Testing Kit 1.0";
 <head>
 	<title><?=$testHead?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel='shortcut icon' type='image/x-icon' href='../favicon.ico' />
+	<link rel='shortcut icon' type='image/x-icon' href='assets/images/logiks.png' />
 
-	<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+	<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
 
 	<meta http-equiv='content-language' content='en' />
-	<meta http-equiv='cache-control' content='no-store, must-revalidate, post-check=0, pre-check=0'>	
-	<meta http-equiv='pragma' content='no-cache'>
+	<meta http-equiv='cache-control' content='no-store, must-revalidate, post-check=0, pre-check=0' />
+	<meta http-equiv='pragma' content='no-cache' />
 </head>
 <body>
-Testing 
+
 </body>
+<script>
+$(function() {
+
+});
+</script>
 </html>
